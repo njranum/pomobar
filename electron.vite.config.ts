24d69c45c293acec7809965c675e504681c7361f
Nicholas Ranum <snoopy@MacBookAir.Home>
2026-05-26 21:48:32 +0100

    Add @/ alias to create relative import path to src/

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -3,11 +3,24 @@ import { defineConfig } from 'electron-vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  main: {},
-  preload: {},
+  main: {
+    resolve: {
+      alias: {
+        '@': resolve('src')
+      }
+    }
+  },
+  preload: {
+    resolve: {
+      alias: {
+        '@': resolve('src')
+      }
+    }
+  },
   renderer: {
     resolve: {
       alias: {
+        '@': resolve('src'),
         '@renderer': resolve('src/renderer/src')
       }
     },
