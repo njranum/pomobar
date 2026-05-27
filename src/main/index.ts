ae6e090def66356e96de339547d966a7cb386a93
Nicholas Ranum <snoopy@MacBookAir.Home>
2026-05-27 21:31:27 +0100

    Creat the popover. Get it opening and closing

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,5 +1,6 @@
 import { app, Tray, nativeImage } from 'electron'
 import { join } from 'path'
+import { createPopover } from './popover'
 
 if (process.platform === 'darwin') {
   app.dock?.hide()
@@ -9,12 +10,32 @@ if (process.platform === 'darwin') {
 let tray: Tray | null = null
 
 app.whenReady().then(() => {
+  // Get the tray icon image
   const iconPath = app.isPackaged
     ? join(process.resourcesPath, 'tray-icon@2x.png')
     : join(__dirname, '../../resources/tray-icon@2x.png')
   const trayIcon = nativeImage.createFromPath(iconPath)
   trayIcon.setTemplateImage(true)
 
+  // Create the tray
   tray = new Tray(trayIcon)
   tray.setToolTip('pomobar')
+
+  // Create the main popoveru
+  const popover = createPopover()
+
+  // Wire the popover to open /close when tray icon isPackaged
+  tray.on('click', (_event, bounds) => {
+    if (popover.isVisible()) {
+      popover.hide()
+      return
+    }
+    //
+    const popoverBounds = popover.getBounds()
+    const x = Math.round(bounds.x - popoverBounds.width / 2 + bounds.width / 2)
+    const y = Math.round(bounds.y + bounds.height)
+    popover.setPosition(x, y)
+    popover.show()
+    popover.focus()
+  })
 })
---
+++ src/main/popover.ts
@@ -0,0 +1,42 @@
+import { BrowserWindow } from 'electron'
+import path from 'path'
+
+let popover: BrowserWindow | null = null
+
+export function createPopover(): BrowserWindow {
+  popover = new BrowserWindow({
+    width: 320,
+    height: 480,
+    show: false,
+    frame: false,
+    resizable: false,
+    movable: false,
+    minimizable: false,
+    maximizable: false,
+    skipTaskbar: true,
+    alwaysOnTop: true,
+    webPreferences: {
+      preload: path.join(__dirname, '../preload/index.ts'),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  })
+
+  // Load renderer
+  if (process.env['ELECTRON_RENDERER_URL']) {
+    // dev
+    popover.loadURL(process.env['ELECTRON_RENDERER_URL'])
+  } else {
+    // prod
+    popover.loadFile(path.join(__dirname, '../renderer/index.html'))
+  }
+
+  // hide popover if it is open and you click off the bounds
+  popover.on('blur', () => {
+    if (popover?.isVisible()) {
+      popover.hide()
+    }
+  })
+
+  return popover
+}
